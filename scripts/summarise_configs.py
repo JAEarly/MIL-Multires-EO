186034f2434126fc3dce4202d5ef993d0b371fbc
Joseph Early <je5g15@soton.ac.uk>
2022-12-22 20:44:27 +0000

    Minor change to config summary script.

--- scripts/summarise_configs.py
+++ scripts/summarise_configs.py
@@ -28,7 +28,7 @@ def summarise_configs():
             "{:d} x {:d}".format(patch_details.grid_size, patch_details.grid_size),
             "{:d} x {:d} px".format(patch_details.cell_size, patch_details.cell_size),
             "{:d} x {:d} px".format(patch_details.patch_size, patch_details.patch_size),
-            "{:d} x {:d} px".format(patch_details.effective_resolution, patch_details.effective_resolution),
+            "{:d} x {:d} px".format(patch_details.effective_patch_resolution, patch_details.effective_patch_resolution),
             "{:.1f}\\%".format(patch_details.scale * 100),
             "{:s}".format(_format_n_params(get_n_params(model_type))),
         ]
@@ -54,13 +54,17 @@ def _format_model_type(model_type):
         return 'ResNet18'
     elif 'unet' in model_type:
         return 'UNet {:s}'.format(model_type[-3:])
+    elif model_type == 'multi_res_single_out':
+        return 'S2P Multi Res Single Out'
+    elif model_type == 'multi_res_multi_out':
+        return 'S2P Multi Res Multi Out'
     grid_size, patch_size = model_type.split('_')
-    return 'MIL {:s} {:s}'.format(patch_size.title(), grid_size)
+    return 'S2P Single Res {:s} {:s}'.format(patch_size.title(), grid_size)
 
 
 def _format_n_params(n_params):
     if n_params >= 1e6:
-        return "{:.1f}M".format(n_params/1e6)
+        return "{:.2f}M".format(n_params/1e6)
     else:
         return "{:.0f}K".format(n_params/1e3)
 
