cb4d492aef0ebcf007d09057da022bef6cc40ef3
Joseph Early <je5g15@soton.ac.uk>
2023-02-13 15:17:02 +0000

    Initial conf mat plotting script done.

--- scripts/plot_conf_mats.py
+++ scripts/plot_conf_mats.py
@@ -1,22 +1,90 @@
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+import csv
 
-# Row normalise
-# import torch.nn.functional as f
-# norm_test_conf_mat = f.normalize(mean_test_conf_mat, p=1, dim=1)
-# norm_test_conf_mat = torch.nan_to_num(norm_test_conf_mat, 0)
-#
-# fig, axis = plt.subplots(nrows=1, ncols=1)
-# sns.heatmap(norm_test_conf_mat, ax=axis, fmt='', cbar=False, cmap='viridis',
-#             vmin=0, vmax=torch.max(norm_test_conf_mat))
-# axis.set_aspect('equal')
-# axis.tick_params(axis='x', which='major', top=False, bottom=True, labeltop=False, labelbottom=True)
-# axis.tick_params(axis='y', which='major', left=False, right=True, labelleft=False, labelright=True,
-#                  labelrotation=0)
-# axis.set_xlabel('Predicted Label')
-# axis.yaxis.set_label_position("right")
-# axis.set_ylabel('True Label')
-# axis.set_facecolor("black")
-# plt.show()
+import matplotlib.pyplot as plt
+# Need to keep scienceplots imported for matplotlib styling even though the import is never used directly
+# noinspection PyUnresolvedReferences
+import scienceplots
+import seaborn as sns
+import torch
+import torch.nn.functional as f
 
-# per_class_recall = torch.diag(mean_test_conf_mat) / torch.sum(mean_test_conf_mat, dim=1)
+plt.style.use(['science', 'bright'])
+
+
+def run():
+    floodnet_path = "results/FloodNet/conf_mats.txt"
+
+    floodnet_model_names, floodnet_conf_mats = parse_conf_mats(floodnet_path, 10)
+    plot_conf_mats(floodnet_model_names, floodnet_conf_mats)
+
+
+def parse_conf_mats(path, n_classes):
+    model_names = []
+    conf_mats = []
+    with open(path, 'r') as conf_mat_file:
+        reader = csv.reader(conf_mat_file, delimiter='|')
+        c = 0
+        for line in reader:
+            if c == 0:
+                model_name = line[0]
+                conf_mat = torch.zeros((n_classes, n_classes))
+            else:
+                if c % 2 == 0:
+                    row = [float(l.strip()) for l in line[1:-1]]
+                    conf_mat[(c - 1) // 2, :] = torch.as_tensor(row)
+            c += 1
+            if c == n_classes * 2 + 2:
+                c = 0
+                model_names.append(model_name)
+                conf_mats.append(conf_mat)
+    return model_names, conf_mats
+
+
+def plot_conf_mats(floodnet_model_names, conf_mats):
+    # Row normalise
+
+    fig, axes = plt.subplots(nrows=2, ncols=5, figsize=(13, 5.5))
+
+    for i in range(2):
+        row_norm_test_conf_mat = f.normalize(conf_mats[i], p=1, dim=1)
+        plot_conf_mat(axes[0][i], row_norm_test_conf_mat)
+        plot_per_class_recall(axes[1][i], conf_mats[i])
+        axes[0][i].set_title(floodnet_model_names[i], size=15)
+
+
+    axes[0][0].set_ylabel('True Label', size=14)
+    axes[1][0].set_ylabel('Recall', size=14)
+
+    axes[0][2].set_xlabel('Predicted Label', size=13)
+    axes[1][2].set_xlabel('Class', size=13)
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_conf_mat(axis, conf_mat, vmax=None, label_x=True, label_y=True):
+    sns.heatmap(conf_mat, ax=axis, fmt='', cbar=False, cmap='viridis',
+                vmin=0, vmax=vmax if vmax is not None else torch.max(conf_mat))
+    axis.set_aspect('equal')
+    axis.tick_params(axis='x', which='both', top=False, bottom=False, labeltop=False, labelbottom=label_x)
+    axis.tick_params(axis='y', which='both', left=False, right=False, labelleft=label_y, labelright=False,
+                     labelrotation=0)
+    # axis.set_xlabel('Predicted Label', size=13)
+    # axis.set_ylabel('True Label', size=14)
+
+
+def plot_per_class_recall(axis, conf_mat):
+    per_class_recall = torch.diag(conf_mat) / torch.sum(conf_mat, dim=1)
+    xs = list(range(len(per_class_recall)))
+    axis.bar(xs, per_class_recall)
+    axis.tick_params(axis='x', which='both', top=False, bottom=False, labeltop=False, labelbottom=True)
+    axis.set_xticks(xs)
+    # axis.set_xlabel('Class', size=13)
+    # axis.set_ylabel('Recall', size=14)
+    axis.set_xlim(-0.6, 9.6)
+    axis.set_ylim(0, 1)
+
+
+if __name__ == "__main__":
+    run()
